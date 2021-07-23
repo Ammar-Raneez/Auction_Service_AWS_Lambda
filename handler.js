dd16c7828430b4111e65af0c667eff19071cddb5
Ammar <ammarraneez@gmail.com>
2021-07-23 10:05:12 +0530

    created initial hello app

--- handler.js
+++ handler.js
@@ -1,15 +1,13 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.hello = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v1.0! Your function executed successfully!',
+            input: event,
+        }, ),
+    };
+
+    callback(null, response);
 };
