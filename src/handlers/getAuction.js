0b3993a3c1d39a03537c9ac9615fc09d389cbd8f
Ammar <ammarraneez@gmail.com>
2021-07-23 21:59:32 +0530

    get single auction

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -7,29 +7,35 @@ const createError = require('http-errors');
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-const getAuctions = async(event, context, callback) => {
-    let auctions;
+const getAuction = async(event, context, callback) => {
+    let auction;
+    const { id } = event.pathParameters;
 
     try {
-        const result = await dynamoDB.scan({
+        const result = await dynamoDB.get({
             TableName: process.env.DB_NAME,
-        }).promise()
+            Key: { id }
+        }).promise();
 
-        auctions = result.Items;
+        auction = result.Item;
     } catch (err) {
         console.error(err);
         throw new createError.InternalServerError(err);
     }
 
+    if (!auction) {
+        throw new createError.NotFound(`Auction with ${id} not found`)
+    }
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify(auctions)
+        body: JSON.stringify(auction)
     }
 
     return response;
 }
 
-module.exports.handler = middy(getAuctions)
+module.exports.handler = middy(getAuction)
     .use(httpJsonBodyParser())
     .use(httpEventNormalizer())
     .use(httpErrorHandler())
