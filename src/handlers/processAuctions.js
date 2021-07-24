6b6c14473d87a797773b0481b809d8f0c202c75b
Ammar <ammarraneez@gmail.com>
2021-07-24 20:56:37 +0530

    close auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,3 +1,17 @@
-module.exports.handler = (event, context, callback) => {
-    console.log("processing auctions");
+const createError = require('http-errors');
+const { getToCloseAuctions } = require('../../lib/getToCloseAuctions');
+const { closeAuctions } = require("../../lib/closeAuctions")
+
+module.exports.handler = async(event, context, callback) => {
+    try {
+        const auctionsToClose = await getToCloseAuctions();
+        const auctionsToClosePromises = auctionsToClose.map(auction => closeAuctions(auction));
+        // wait till all auctions are closed
+        await Promise.all(auctionsToClosePromises);
+
+        return { amountClosed: auctionsToClosePromises.length };
+    } catch (err) {
+        console.error(err);
+        throw new createError.InternalServerError(err);
+    }
 }
