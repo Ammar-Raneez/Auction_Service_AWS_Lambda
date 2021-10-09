45eb500d755dedc275be04dd1dd08ed006a8cf13
ammar <ammarraneez@gmail.com>
2021-10-09 18:37:13 +0530

    refactor: notification service

--- Notification Service/src/handlers/hello.js
+++ Notification Service/src/handlers/hello.js
@@ -4,12 +4,12 @@ module.exports.handler = async (event) => {
 	return {
 		statusCode: 200,
 		body: JSON.stringify(
-		{
-			message: 'Go Serverless v1.0! Your function executed successfully!',
-			input: event,
-		},
-		null,
-		2
+			{
+				message: 'Go Serverless v1.0! Your function executed successfully!',
+				input: event,
+			},
+			null,
+			2,
 		),
 	};
 };
--- Notification Service/src/handlers/sendMail.js
+++ Notification Service/src/handlers/sendMail.js
@@ -2,43 +2,48 @@
 
 const AWS = require('aws-sdk');
 const ses = new AWS.SES({
-    region: 'us-east-1'
-})
+	region: 'eu-west-1',
+});
 
 module.exports.handler = async (event) => {
-    // there's only a single record due to batch size of 1
-    const record = event.Records[0];
+	console.log(event);
 
-    const email = JSON.parse(record.body);
-    const { subject, body, recipient } = email;
+	// the records are in Records array
+	// there's only a single record due to batch size of 1
+	const record = event.Records[0];
 
-    const params = {
-        // the email that is verified in SES
-        Source: 'ammarraneez@gmail.com',
-        Destination: {
-            // array of recipients
-            ToAddresses: [recipient]
-        },
-        Message: {
-            Body: {
-                // html can also be used for body
-                Text: {
-                    Data: body
-                }
-            },
-            Subject: {
-                Data: subject
-            }
-        }
-    }
+	// each record has a body, and sqs accepts only strings
+	// so we will need to parse it to get is as JSON
+	const email = JSON.parse(record.body);
+	const { subject, body, recipient } = email;
+
+	const params = {
+		// the email that is verified in SES
+		Source: 'amatendevs@gmail.com',
+		Destination: {
+			// array of recipients
+			ToAddresses: [recipient]
+		},
+		Message: {
+			Body: {
+				// html can also be used for body
+				Text: {
+					Data: body,
+				}
+			},
+			Subject: {
+				Data: subject,
+			}
+		}
+	};
 
     try {
-        const result = await ses.sendEmail(params)
-            .promise();
-        return result;
+			const result = await ses.sendEmail(params)
+				.promise();
+			return result;
     } catch (err) {
-        // this function isn't triggered by API Gateway
-        // so, and error object doesn't have to be returned
-        console.error(err);
+			// this function isn't triggered by API Gateway
+			// so, an error http object doesn't have to be returned
+			console.error(err);
     }
 };
